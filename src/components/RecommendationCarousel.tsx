3fdc6f79b059464f80ec6777f4cbc68147abe2d0
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2025-05-09 15:41:59 +0100

    Updated recommendation avatars

--- src/components/RecommendationCarousel.tsx
+++ src/components/RecommendationCarousel.tsx
@@ -7,9 +7,9 @@ const RecomendationCarousel: FC<ICreditDataProps> = ({ cast }) => {
   const navigate = useNavigate();
   const ifTV = window.location.href.includes("tv");
   const handleClick = (id: number) => {
-    navigate(`${ifTV ? '/details/tv/' : '/details/'}${id}`, { replace: true });
+    navigate(`${ifTV ? "/details/tv/" : "/details/"}${id}`, { replace: true });
     window.location.reload();
-    window.scrollTo({ top: 0, left: 0})
+    window.scrollTo({ top: 0, left: 0 });
   };
   return (
     <div className="flex overflow-x-scroll pb-10 hide-scroll-bar mt-4">
@@ -18,12 +18,12 @@ const RecomendationCarousel: FC<ICreditDataProps> = ({ cast }) => {
           <button onClick={() => handleClick(actor?.id)} key={actor?.id}>
             <div className="rounded shadow-lg px-3 w-[148px]">
               <img
-                className="w-full h-[140px]"
                 src={
                   actor.poster_path
                     ? `https://image.tmdb.org/t/p/original${actor?.poster_path}`
                     : "https://www.snapon.co.za/images/thumbs/default-image_550.png"
                 }
+                className="mx-auto mb-4 w-32 rounded-lg"
                 alt={actor.title}
               />
               <div className="py-2">
